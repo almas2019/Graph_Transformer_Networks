7a5f4f2b74661c7c7b3276c94a4e451ef60ac949
Almas K <almas.khan@mail.utoronto.ca>
2024-05-02 11:55:47 -0700

    uptd plot loss heatmap 500 epoch

--- run_model/heatmap.py
+++ run_model/heatmap.py
@@ -1,7 +1,8 @@
 import torch
 import altair as alt
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 def load_layer_tensors(output_folder, num_layers, split_type, run_number):
     layer_tensors = []
 
@@ -34,6 +35,7 @@ def create_heatmap(layer_averages, labels, output_folder, split_type, run_number
     # Convert the "Label" column to a categorical data type with the specified order
     df['Label'] = pd.Categorical(df['Label'], categories=labels, ordered=True)
 
+    # Altair heatmap
     heatmap = alt.Chart(df).mark_rect().encode(
         x='Layer:O',
         y=alt.Y('Label:O', sort=None),  # Disable alphabetical sorting
@@ -43,8 +45,31 @@ def create_heatmap(layer_averages, labels, output_folder, split_type, run_number
         width=200,
         height=150
     )
-
     heatmap.save(output_folder + f'{split_type}_{run_number}_heatmap_500_epochs.html') # add epoch parameter after
+
+    # Seaborn heatmap
+    plt.figure(figsize=(8, 6))
+    df_pivot = df.pivot(index='Label', columns='Layer', values='Value')  # Pivot the DataFrame
+    sns.heatmap(data=df_pivot,
+                cmap='Blues',
+                cbar_kws={'label': 'Value'},
+                annot=True,
+                fmt=".2f")
+    
+    # Rotate y-axis labels to make them horizontal
+    plt.yticks(rotation=0)
+    plt.title('Heatmap of Filters')
+    plt.xlabel('Layer')
+    plt.ylabel('Label')
+
+    # Save as PNG
+    plt.savefig(output_folder + f'heatmap_epoch_500_{split_type}_{run_number}.png')
+
+    # Save as SVG
+    plt.savefig(output_folder + f'heatmap_epoch_500_{split_type}_{run_number}.svg', format='svg')
+
+    # Clear the plot for the next iteration
+    plt.clf()
     df.to_csv(output_folder + f'{split_type}_{run_number}_layer_avg_channel_500_epochs.csv')
 
 data_folder = "/home/almas/projects/def-gregorys/almas/Graph_Transformer_Networks/data/"
@@ -63,10 +88,10 @@ labels_graph_het_subsampled_tcell_fib5 = ['t-t', 'f-f', 'bridge', 't-f', 'I']
 labels_graph_het_subsampled_tcell_fib3 = ['t-t', 'f-f', 't-f','I']
 labels_graph_het_subsampled_tcell_fib2 = ['t-t', 'f-f', 't-f','f-t','I']
 labels_graph_het_subsampled_tcell_fib = ['t-t', 'f-f', 't-f','I']
-run_for_each_model("IMDB", labels_imdb, 3, "train", 10)
-run_for_each_model("DBLP", labels_dblp, 4, "train", 10)
-run_for_each_model("ACM", labels_acm, 3, "train", 10)
-run_for_each_model("graph_het_subsampled_tcell_fib4", labels_graph_het_subsampled_tcell_fib4, 2, "train", 10)
+run_for_each_model("IMDB", labels_imdb, 4, "train", 1)
+run_for_each_model("DBLP", labels_dblp, 4, "train", 1)
+run_for_each_model("ACM", labels_acm, 3, "train", 1)
+run_for_each_model("graph_het_subsampled_tcell_fib4", labels_graph_het_subsampled_tcell_fib4, 2, "train", 1)
 run_for_each_model("graph_het_subsampled_tcell_fib5", labels_graph_het_subsampled_tcell_fib5, 2, "train", 1)
 run_for_each_model("graph_het_subsampled_tcell_fib3", labels_graph_het_subsampled_tcell_fib3, 2, "train", 1)
 run_for_each_model("graph_het_subsampled_tcell_fib2", labels_graph_het_subsampled_tcell_fib2, 2, "train", 1)
